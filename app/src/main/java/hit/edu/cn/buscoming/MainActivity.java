8c59eec85f60b2e866eab2b38a00095e040ea1fc
rccoder <rccoder.net@gmail.com>
2016-07-07 12:02:57 +0800

    Add login Activity

--- app/src/main/java/hit/edu/cn/buscoming/MainActivity.java
+++ app/src/main/java/hit/edu/cn/buscoming/MainActivity.java
@@ -1,5 +1,6 @@
 package hit.edu.cn.buscoming;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -82,10 +83,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_often) {
-
-        } else if (id == R.id.nav_star) {
-
+        } else if (id == R.id.nav_login) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
