7d47f9c4b7c3748957884fc2c363a4a2ad1a0b52
fateice <k314874957@outlook.com>
2016-07-12 13:55:38 +0800

    添加FAB

--- app/src/main/java/hit/edu/cn/buscoming/Activity/MainActivity.java
+++ app/src/main/java/hit/edu/cn/buscoming/Activity/MainActivity.java
@@ -2,9 +2,7 @@ package hit.edu.cn.buscoming.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -112,14 +110,15 @@ public class MainActivity extends BaseActivity
         setSupportActionBar(toolbar);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
 
 
 
@@ -131,6 +130,23 @@ public class MainActivity extends BaseActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void fabline (View view)
+    {
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this,LineActivity.class);
+        startActivity(intent);
+    }
+
+    public void fabstop (View view)
+    {
+
+    }
+
+    public void fabdes (View view)
+    {
+
     }
 
     @Override
@@ -184,8 +200,7 @@ public class MainActivity extends BaseActivity
             Intent nav_about_intent = new Intent(MainActivity.this, AboutActivity.class);
             startActivity(nav_about_intent);
         }else if(id == R.id.nav_logout) {
-            Intent intent = new Intent(MainActivity.this, LineActivity.class);
-            startActivity(intent);
+
         }else if (id == R.id.nav_exit) {
             Log.d("Now", "exit");
             ActivityCollector.finishAll();
